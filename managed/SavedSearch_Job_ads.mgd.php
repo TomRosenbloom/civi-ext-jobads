ee9fe10c54ea6356c600879bd5340b9ed278d61e
tom <tom@voscur.org>
2023-12-13 16:51:24 +0000

    added packaged search

---
+++ managed/SavedSearch_Job_ads.mgd.php
@@ -0,0 +1,49 @@
+<?php
+
+use CRM_Jobads_ExtensionUtil as E;
+
+
+return [
+  [
+    'name' => 'SavedSearch_Job_ads',
+    'entity' => 'SavedSearch',
+    'cleanup' => 'always',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'Job_ads',
+        'label' => E::ts('Job ads'),
+        'api_entity' => 'JobAd',
+        'api_params' => [
+          'version' => 4,
+          'select' => [
+            'id',
+            'JobAd_Contact_contact_id_01.sort_name',
+            'job_title',
+            'summary',
+            'description',
+          ],
+          'orderBy' => [],
+          'where' => [],
+          'groupBy' => [],
+          'join' => [
+            [
+              'Contact AS JobAd_Contact_contact_id_01',
+              'LEFT',
+              [
+                'contact_id',
+                '=',
+                'JobAd_Contact_contact_id_01.id',
+              ],
+            ],
+          ],
+          'having' => [],
+        ],
+      ],
+      'match' => [
+        'name',
+      ],
+    ],
+  ],
+];

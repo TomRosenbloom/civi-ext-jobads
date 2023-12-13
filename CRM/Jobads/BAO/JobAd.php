318db0af7237aa20a56fd5018f92f01d32b13ddc
tom <tom@voscur.org>
2023-12-13 13:34:36 +0000

    generated entity

---
+++ CRM/Jobads/BAO/JobAd.php
@@ -0,0 +1,8 @@
+<?php
+// phpcs:disable
+use CRM_Jobads_ExtensionUtil as E;
+// phpcs:enable
+
+class CRM_Jobads_BAO_JobAd extends CRM_Jobads_DAO_JobAd {
+
+}
---
+++ Civi/Api4/JobAd.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * JobAd entity.
+ *
+ * Provided by the jobads extension.
+ *
+ * @package Civi\Api4
+ */
+class JobAd extends Generic\DAOEntity {
+
+}

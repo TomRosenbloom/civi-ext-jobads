939c90e00c3998cffc22d1854d32ae4fa4df6a0e
TomRosenbloom <TomRosenbloom@users.noreply.github.com>
2023-12-21 17:09:35 +0000

    jobads contact tab working

--- CRM/Jobads/Page/ContactTab.php
+++ CRM/Jobads/Page/ContactTab.php
@@ -5,12 +5,30 @@ class CRM_Jobads_Page_ContactTab extends CRM_Core_Page {
 
   public function run() {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(E::ts('ContactTab'));
+    CRM_Utils_System::setTitle(E::ts('My Entity'));
+    $contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this, TRUE);
+    $myEntities = \Civi\Api4\MyEntity::get()
+      ->select('*')
+      ->addWhere('contact_id', '=', $contactId)
+      ->execute();
+    $rows = array();
+    foreach($myEntities as $myEntity) {
+      $row = $myEntity;
+      if (!empty($row['contact_id'])) {
+        $row['contact'] = '<a href="'.CRM_Utils_System::url('civicrm/contact/view', ['reset' => 1, 'cid' => $row['contact_id']]).'">'.CRM_Contact_BAO_Contact::displayName($row['contact_id']).'</a>';
+      }
+      $rows[] = $row;
+    }
+    $this->assign('contactId', $contactId);
+    $this->assign('rows', $rows);
 
-    // Example: Assign a variable for use in a template
-    $this->assign('currentTime', date('Y-m-d H:i:s'));
+    // Set the user context
+    $session = CRM_Core_Session::singleton();
+    $userContext = CRM_Utils_System::url('civicrm/contact/view', 'cid='.$contactId.'&selectedChild=contact_my_entity&reset=1');
+    $session->pushUserContext($userContext);
 
     parent::run();
   }
+
 
 }
--- managed/SavedSearch_Job_ads.mgd.php
+++ managed/SavedSearch_Job_ads.mgd.php
@@ -44,4 +44,72 @@ return [
       ],
     ],
   ],
+  [
+    'name' => 'SavedSearch_Job_ads_SearchDisplay_Job_ads_Table_1',
+    'entity' => 'SearchDisplay',
+    'cleanup' => 'always',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'Job_ads_Table_1',
+        'label' => E::ts('Job ads Table 1'),
+        'saved_search_id.name' => 'Job_ads',
+        'type' => 'table',
+        'settings' => [
+          'description' => NULL,
+          'sort' => [],
+          'limit' => 50,
+          'pager' => [],
+          'placeholder' => 5,
+          'columns' => [
+            [
+              'type' => 'field',
+              'key' => 'id',
+              'dataType' => 'Integer',
+              'label' => E::ts('ID'),
+              'sortable' => TRUE,
+            ],
+            [
+              'type' => 'field',
+              'key' => 'JobAd_Contact_contact_id_01.sort_name',
+              'dataType' => 'String',
+              'label' => E::ts('Job Ad Contact: Sort Name'),
+              'sortable' => TRUE,
+            ],
+            [
+              'type' => 'field',
+              'key' => 'job_title',
+              'dataType' => 'String',
+              'label' => E::ts('Job title'),
+              'sortable' => TRUE,
+            ],
+            [
+              'type' => 'html',
+              'key' => 'summary',
+              'dataType' => 'Text',
+              'label' => E::ts('Summary'),
+              'sortable' => TRUE,
+            ],
+            [
+              'type' => 'html',
+              'key' => 'description',
+              'dataType' => 'Text',
+              'label' => E::ts('Full description'),
+              'sortable' => TRUE,
+            ],
+          ],
+          'actions' => TRUE,
+          'classes' => [
+            'table',
+            'table-striped',
+          ],
+        ],
+      ],
+      'match' => [
+        'saved_search_id',
+        'name',
+      ],
+    ],
+  ],
 ];

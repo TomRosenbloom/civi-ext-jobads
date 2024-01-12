f97995c2fb8bdbbf8bbaadae65c516e179008e9a
TomRosenbloom <TomRosenbloom@users.noreply.github.com>
2024-01-12 18:19:36 +0000

    Removed extra job ads tab

--- jobads.php
+++ jobads.php
@@ -31,29 +31,3 @@ function jobads_civicrm_install(): void {
 function jobads_civicrm_enable(): void {
   _jobads_civix_civicrm_enable();
 }
-
-/**
- * Implementation of hook_civicrm_tabset
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_tabset
- */
-function jobads_civicrm_tabset($path, &$tabs, $context) {
-  if ($path === 'civicrm/contact/view') {
-    // add a tab to the contact summary screen
-    $contactId = $context['contact_id'];
-    $url = CRM_Utils_System::url('civicrm/jobads/contacttab', ['cid' => $contactId]);
-
-    $myEntities = \Civi\Api4\JobAd::get()
-      ->selectRowCount()
-      ->addWhere('contact_id', '=', $contactId)
-      ->execute();
-
-    $tabs[] = array(
-      'id' => 'contact_jobads',
-      'url' => $url,
-      'count' => $myEntities->count(),
-      'title' => E::ts('Job Ads'),
-      'weight' => 150,
-      'icon' => 'crm-i fa-envelope-open',
-    );
-  }
-}
